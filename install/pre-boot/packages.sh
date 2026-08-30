f1a4c345cb0c01841b2ee5fba48fe203863a350d
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 17:24:39 +0200

    Update package paths in packages.sh script

--- install/pre-boot/packages.sh
+++ install/pre-boot/packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-base_packages="$INSTALL_DIR/packages/base.packages"
-extra_packages="$INSTALL_DIR/packages/extra.packages"
+base_packages="/tmp/my-archy/install/pre-boot/base.packages"
+extra_packages="/tmp/my-archy/install/pre-boot/extra.packages"
 
 yay -S --noconfirm --needed - < "$base_packages"
 yay -S --noconfirm --needed - < "$extra_packages"
