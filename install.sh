38492623ee66f906b6d310637a8f6a86a7c7b8c9
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 12:57:49 +0200

    Change clone directory for my-archy repository

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ echo "Updating system and installing git..."
 sudo pacman -Syu --noconfirm --needed git
 
 echo -e "\nCloning my-archy repository..."
-git clone https://github.com/Thijzert123/my-archy.git ~/.local/share/my-archy
+git clone https://github.com/Thijzert123/my-archy.git /tmp/my-archy
 
 echo -e "\nStarting installation..."
-source ~/.local/share/my-archy/install/all.sh
+source /tmp/my-archy/install/all.sh
