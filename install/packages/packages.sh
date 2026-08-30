d25e7324a734c70aee05fa1016a137ea957dba07
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 13:20:05 +0200

    Update package installation to use --noconfirm option

--- install/packages/packages.sh
+++ install/packages/packages.sh
@@ -3,5 +3,5 @@
 base_packages="$INSTALL_DIR/packages/base.packages"
 extra_packages="$INSTALL_DIR/packages/extra.packages"
 
-yay -S --needed - < "$base_packages"
-yay -S --needed - < "$extra_packages"
+yay -S --noconfirm --needed - < "$base_packages"
+yay -S --noconfirm --needed - < "$extra_packages"
