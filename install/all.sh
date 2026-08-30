07e0000bd5fe15e1d88252c6c939689a032ae50e
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 12:52:18 +0200

    Enhance all.sh with comments for clarity
    Added comments to clarify script behavior and ensure updates.

--- install/all.sh
+++ install/all.sh
@@ -5,6 +5,9 @@ set -eEo pipefail
 
 sudo -v
 
+# To have -y onece more just to be sure (if this script directly gets called)
+sudo pacman -Syu --noconfirm
+
 export INSTALL_DIR="~/.local/share/my-archy/install"
 
 source "$INSTALL_DIR/packages/all.sh"
