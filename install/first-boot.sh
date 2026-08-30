6b3c7ff2b726d90b2c2239294c22c0beed0d5708
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 19:43:17 +0200

    Refactor first-boot.sh to loop through users

--- install/first-boot.sh
+++ install/first-boot.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-USERNAME="$(
-    getent passwd |
-        awk -F: '
-            $3 >= 1000 &&
-            $3 < 60000 &&
-            $1 != "nobody" {
-                print $1
-                exit
-            }
-        '
-)"
-
-runuser -u "$USERNAME" -- /opt/my-archy/install/user-setup.sh
+# Loop all normal users to install their setup
+getent passwd |
+    awk -F: '$3 >= 1000 && $3 < 60000 && $1 != "nobody" { print $1 }' |
+    while IFS= read -r USERNAME; do
+        runuser -u "$USERNAME" -- /opt/my-archy/install/user-setup.sh
+    done
 
 systemctl disable my-archy-first-boot.service
