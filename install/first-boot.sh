6f2d7e5f19c56f285ab6bc492c5cad693be06e6f
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 17:51:10 +0200

    Update first-boot.sh to run user setup
    Retrieve the first valid username and run user setup script.

--- install/first-boot.sh
+++ install/first-boot.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+USERNAME="$(
+    getent passwd |
+        awk -F: '
+            $3 >= 1000 &&
+            $3 < 60000 &&
+            $1 != "nobody" {
+                print $1
+                exit
+            }
+        '
+)"
+
+runuser -u "$USERNAME" -- /opt/my-archy/install/user-setup.sh
+
+systemctl disable my-archy-first-boot.service
