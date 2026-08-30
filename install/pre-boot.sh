056a82ae129d8d3199e0bd68e12cb8460b532ced
Thijzert <184778919+Thijzert123@users.noreply.github.com>
2026-08-30 17:25:55 +0200

    Implement cleanup for temporary sudoers file
    Add cleanup function for temporary sudoers file.

--- install/pre-boot.sh
+++ install/pre-boot.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
-source "$INSTALL_DIR/packages/yay.sh"
-source "$INSTALL_DIR/packages/packages.sh"
-source "$INSTALL_DIR/packages/rust.sh"
+TEMP_SUDO="/etc/sudoers.d/my-archy-temp"
+
+cleanup() {
+    rm -f "$TEMP_SUDO"
+}
+
+trap cleanup EXIT
+
+USERNAME="$(
+    getent passwd |
+        awk -F: '
+            $3 >= 1000 &&
+            $3 < 60000 &&
+            $1 != "nobody" {
+                print $1
+                exit
+            }
+        '
+)"
+
+runuser -u "$USERNAME" -- "/tmp/my-archy/install/pre-boot/yay.sh"
+runuser -u "$USERNAME" -- "/tmp/my-archy/install/pre-boot/packages.sh"
+runuser -u "$USERNAME" -- "/tmp/my-archy/install/pre-boot/rust.sh"
