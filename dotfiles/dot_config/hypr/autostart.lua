3dfd0964c3b91a0826b514f02b3bbe6f0669c51e
Thijzert123 <184778919+Thijzert123@users.noreply.github.com>
2026-08-31 17:06:38 +0200

    Add hyprpaper

--- dotfiles/dot_config/hypr/autostart.lua
+++ dotfiles/dot_config/hypr/autostart.lua
@@ -3,6 +3,7 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("systemctl --user import-environment $(env | cut -d'=' -f 1)")
   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
 
+  hl.exec_cmd("uwsm app -- hyprpaper")
   hl.exec_cmd("uwsm app -- walker --gapplication-service")
   hl.exec_cmd("uwsm app -- waybar")
 end)
